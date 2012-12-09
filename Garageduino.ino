00a41a4353ee8e882cc53a1e83df22ef31fd5fb5
Bran Sorem <bransorem@gmail.com>
2012-12-08 19:50:02 -0800

    reworking the way commands are done

--- Garageduino.ino
+++ Garageduino.ino
@@ -2,21 +2,25 @@
 
 char ssid[] = "SSID";          //  your network SSID (name) 
 char pass[] = "password";    // your network password
-int PORT = 8080;              // port (set up port forwarding in your router)
+int PORT = 8080;             // any port you choose
 WiFiServer server(PORT);
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-String state = "off";  // default state of LED, DELETE if not needed
-
 byte IP = 9;
 byte LED = 8;
+byte SENSOR = 4;
+byte SWITCH = 2;
 
 void setup() {
   // initialize serial:
   Serial.begin(115200);
+  Serial.println("Starting Garageduino");
   
   pinMode(LED, OUTPUT);
   pinMode(IP, OUTPUT);
+  pinMode(SENSOR, INPUT); //set sensor pin as input for door sensor
+  pinMode(SWITCH, OUTPUT); 
+  digitalWrite(SWITCH, HIGH);
   
   byte MAC[6];
   WiFi.macAddress(MAC);
@@ -33,10 +37,13 @@ void setup() {
     Serial.println(myAddress);
     digitalWrite(IP, HIGH);
   }
+  
+  
 }
 
 void loop() {
   // listen for incoming clients
+  
   WiFiClient client = server.available();
   if (client) {
     // an http request ends with a blank line
@@ -62,7 +69,7 @@ String getcmd(WiFiClient client) {
   String command = "";
   String response = "";
   String temp = "";
-  String headers[20];
+  String headers[20];  // up to 20 HTTP headers
   int count = 0;
   char in;
   while(client.available()) { // read full string
@@ -79,8 +86,6 @@ String getcmd(WiFiClient client) {
       command += in;
     }
   }
-  
-//  return response;
 
   // Way to parse and return individual headers stored in 'headers' array
   String header = "";
@@ -111,21 +116,30 @@ void send_response(WiFiClient client, String response) {
 String checkcmd(String cmd){
   String response = "";
   
+  String header1 = "CUSTOM";
+  
   // header with request to turn on led
-  if (cmd.startsWith("Scorgan: on")) {
-    digitalWrite(8, HIGH);
-    state = "on";
-    response = state;
-  }
-  // header with request to turn off led
-  else if (cmd.startsWith("Scorgan: off")) {
-    digitalWrite(8, LOW);
-    state = "off";
-    response = state;
-  }
-  else if (cmd.startsWith("Scorgan: status")) {
-    int a = 58;  // or use some actual sensor data
-    response = String(a);
+  if (cmd.startsWith(header1)){
+    String tmp = cmd.substring(header1.length() + 1, cmd.length());
+    
+    if (tmp.compareTo("toggle")){
+      digitalWrite(SWITCH, LOW);
+      delay(300);
+      digitalWrite(SWITCH, HIGH);
+    }
+    if (tmp.compareTo("status")){
+      int a;
+      
+      if (digitalRead(SENSOR) == HIGH) {
+        a = 1;  //DOOR CLOSED
+      }
+      else {
+        a = 0;  //DOOR OPEN
+      }
+      
+      //int a = digitalRead(SENSOR);
+      response = String(a);
+    }
   }
   
   return response;
